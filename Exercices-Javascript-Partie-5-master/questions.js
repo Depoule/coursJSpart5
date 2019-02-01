02bba9c1f5ded0608b13ee0a2111e685d39c4cd7
Depoule <jpoulet02100@gmail.com>
2019-02-01 11:32:19 +0100

    partie 5 fini et corrigé

---
+++ Exercices-Javascript-Partie-5-master/questions.js
@@ -0,0 +1,55 @@
+var languagesArrayCreation = function () {
+  var languages = ['Html', 'CSS', 'Java', 'PHP'];
+  return languages;
+}
+
+var numbersArrayCreation = function () {
+    var numbers = [0, 1, 2, 3, 4, 5];
+    return numbers;
+}
+
+var ElementReplacement = function (languages) {
+  languages[2] = 'Javascript';
+  return languages;
+}
+
+var AddElementToLanguagesArray = function (languages) {
+  languages.push('Ruby', 'Python');
+  return languages;
+}
+
+var AddElementToNumbersArray = function (numbers) {
+  numbers.unshift(-2, -1);
+  return numbers;
+}
+
+var deleteArrayFirstElement = function (languages) {
+  languages.shift();
+  return languages;
+}
+
+var deleteArrayLastElement = function (languages) {
+  languages.splice(4, 1);
+  return languages;
+}
+
+var stringToArray = function (socialMediaInString) {
+  var socialMedia = socialMediaInString.split(',');
+  return socialMedia;
+}
+
+var arrayToString = function (languages) {
+  var languagesInString = languages.join(',');
+  return languagesInString;
+}
+
+var arraySort = function (socialMedia) {
+
+  return socialMedia.sort();
+}
+
+var arrayInvert = function (languages){
+
+
+  return languages.reverse();
+}
